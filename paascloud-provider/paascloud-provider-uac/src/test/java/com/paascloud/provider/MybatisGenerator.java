dbfcd237aeaeef077592cae11fa4ce9035e14b77
huanglei <hlyaowan@163.com>
2018-05-22 12:02:30 +0800

    init

---
+++ paascloud-provider/paascloud-provider-uac/src/test/java/com/paascloud/provider/MybatisGenerator.java
@@ -0,0 +1,30 @@
+package com.paascloud.provider;
+
+
+/**
+ * The class Mybatis generator.
+ * 
+ * @author paascloud.net@gmail.com
+ */
+public class MybatisGenerator {
+
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args
+	 *            the input arguments
+	 *
+	 * @throws Exception
+	 *             the exception
+	 */
+	public static void main(String[] args) throws Exception {
+		// List<String> warnings = new ArrayList<>();
+		// ConfigurationParser cp = new ConfigurationParser(warnings);
+		// Configuration config = cp.parseConfiguration(
+		// Generator.class.getResourceAsStream("/generator/generatorConfig.xml"));
+		// DefaultShellCallback callback = new DefaultShellCallback(true);
+		// MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+		// callback, warnings);
+		// myBatisGenerator.generate(null);
+	}
+}
